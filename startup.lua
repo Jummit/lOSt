917e94f8a8728f80b165ff4990d81658d6904e88
Jummit <jummit@web.de>
2018-05-20 14:56:41 +0200

    add timer to main loop

--- startup.lua
+++ startup.lua
@@ -1,11 +1,18 @@
+-- clear screen white as quick as possible
+term.setBackgroundColor(colors.white)
+term.clear()
+
 -- intended global, so every program can modify the processes
 processes = require "apis.processes"
 processes:start("autostart")
 
 while true do
   -- update
+  local timer = os.startTimer(0.001)
   local event, var1, var2, var3 = os.pullEventRaw()
-  term.setBackgroundColor(colors.white)
-  term.clear()
-  processes:update(processes, event, var1, var2, var3)
+  os.cancelTimer(timer)
+  
+  if var1 ~= timer then
+    processes:update(processes, event, var1, var2, var3)
+  end
 end
