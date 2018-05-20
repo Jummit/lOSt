e6efea007ebb959181a8fae50bce47cb2d9936a5
Jummit <jummit@web.de>
2018-05-20 13:25:36 +0200

    clear after pullEvents so programs can be drawn

--- startup.lua
+++ startup.lua
@@ -3,9 +3,9 @@ processes = require "apis.processes"
 processes:start("init")
 
 while true do
-  term.setBackgroundColor(colors.white)
-  term.clear()
   -- update
   local event, var1, var2, var3 = os.pullEventRaw()
+  term.setBackgroundColor(colors.white)
+  term.clear()
   processes:update(processes, event, var1, var2, var3)
 end
