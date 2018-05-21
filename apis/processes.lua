836f8cadd4c1decae8c2d68531cfc7245bd20ddb
Jummit <jummit@web.de>
2018-05-21 19:34:11 +0200

    limit on resize size

--- apis/processes.lua
+++ apis/processes.lua
@@ -156,9 +156,11 @@ return {
         self.term.reposition(x, y)
       end,
       resize = function(self, w, h)
-        local oldX, oldY = self.term.getPosition()
-        self.term.reposition(oldX, oldY, w, h)
-        os.queueEvent("term_resize")
+        if w>4 and h>3 then
+          local oldX, oldY = self.term.getPosition()
+          self.term.reposition(oldX, oldY, w, h)
+          os.queueEvent("term_resize")
+        end
       end
     }
 
